e7c5c6d16c7ae315b8391686800bf2268c49586a
AleksandrNikiforov <aleksandr040988@gmail.com>
2023-04-10 19:57:16 +0500

    foreignKey

--- homework_07/presalesite/examination/urls.py
+++ homework_07/presalesite/examination/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
-from .views import *
+from .views import CategoryListView, SubcategoryListView, ProductListView, SubproductListView
 
 urlpatterns = [
-    path('', ExaminationListView.as_view(), name='Exam'),
-    path('examination-create/', ExaminationCreateView.as_view(), name='Exam_create'),
+    path('', CategoryListView.as_view(), name='Exam'),
+    path('sub-category/', SubcategoryListView.as_view(), name='Sub-category'),
+    path('sub-category/product/', ProductListView.as_view(), name='Product'),
+    path('sub-category/product/sub-product/', SubproductListView.as_view(), name='Sub-product'),
 ]
--- homework_07/presalesite/examination/views.py
+++ homework_07/presalesite/examination/views.py
@@ -2,16 +2,49 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import *
-from .forms import ExaminationForm
+from .forms import *
 
 
 
-class ExaminationListView(ListView):
-    model = Examination
+class CategoryListView(ListView):
+    model = Category
     form_class = ExaminationForm
 
+    def get_queryset(self):
+        return Category.objects.all()  
 
-class ExaminationCreateView(CreateView):
-    model = Examination
+
+class SubcategoryListView(ListView):
+    model = Subcategory
     form_class = ExaminationForm
-    success_url = '/examination'
+
+    def get_queryset(self):
+        #category_id = self.kwargs['category_id']
+        return Subcategory.objects.filter(subcategory_id = 1) 
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['category_id'] = self.kwargs['category_id']
+    #     return context
+
+
+
+class ProductListView(ListView):
+    model = Product
+    form_class = ExaminationForm
+
+    def get_queryset(self):
+        return Product.objects.filter(category_id = 2) 
+    
+
+class SubproductListView(ListView):
+    model = Subproduct
+    form_class = ExaminationForm
+
+    def get_queryset(self):
+        return Subproduct.objects.filter(category_id = 9)  
+    
+# class ExaminationCreateView(CreateView):
+#     #model = Examination
+#     form_class = ExaminationForm
+#     success_url = '/examination'
