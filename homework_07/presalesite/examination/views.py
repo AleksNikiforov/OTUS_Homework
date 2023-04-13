e9481ee0942c4c4a2064ba3f2ebe53516c236185
AleksandrNikiforov <aleksandr040988@gmail.com>
2023-04-13 21:19:42 +0500

    add java script

--- homework_07/presalesite/examination/views.py
+++ homework_07/presalesite/examination/views.py
@@ -10,14 +10,56 @@ class CategoryListView(ListView):
     model = Category
     form_class = ExaminationForm
 
+    def post(self, request, *args, **kwargs):
+        # получаем данные из POST-запроса
+        if request.method == 'POST':
+            data = request.POST
+            print(dict(data))
+            data = {k: v for k, v in data.items() if v is not None and v != ""}
+            data.pop("csrfmiddlewaretoken")
+            print(data)
+
+
+# def my_view(request):
+#     if request.method == 'POST':
+#         # проверяем, была ли установлена галочка
+#         if request.POST.get('html') == 'on':
+#             # галочка была установлена
+#         else:
+#             # галочка не была установлена
+
+#         # обработка полученных данных...
+
+#     return render(request, 'my_template.html')
+
+
+# def my_view(request):
+#     if request.method == 'POST':
+#         # получаем значение, введенное пользователем
+#         input_value = request.POST.get('float-input')
+
+#         # получаем текст, содержащийся в элементе <label>
+#         label_text = 'Трудозатраты, дней:'
+
+#         # обработка полученных данных...
+
+#     return render(request, 'my_template.html')
+
+
+# def my_view(request):
+#     summary_text = request.POST.get('summary_text')
+
+#     # обработка полученных данных...
+
+#     return render(request, 'my_template.html')
+
+
 
 class SubcategoryListView(ListView):
     model = Subcategory
     form_class = ExaminationForm
     
     def get(self, request, *args, **kwargs):
-        print(kwargs['pk_cat'])
-        print('#'*50)
         self.subcategory_id = kwargs['pk_cat']
         return super().get(request,*args, **kwargs)
 
@@ -31,8 +73,6 @@ class ProductListView(ListView):
 
     def get(self, request, *args, **kwargs):
         self.subcategory_id = kwargs['pk_product']
-        print(self.subcategory_id)
-        print('-'*50)
         return super().get(request,*args, **kwargs)
 
     def get_queryset(self):
